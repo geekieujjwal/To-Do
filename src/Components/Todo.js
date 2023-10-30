f0e7d2eb924dca21c6c88862a92dbb790c0acf25
Ujjwal Jaguri <ujjwal.jaguri.19012003@gmail.com>
2023-10-30 17:54:49 +0530

    added: edit and delete functionality with localstorage

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,22 +1,84 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Todo = () => {
   const [inputText, setInputText] = useState("");
-  const [todoArr, setTodoArr] = useState([]);
+  const [todoArr, setTodoArr] = useState(() => {
+    const storedTodoArr = localStorage.getItem("todos");
+    return storedTodoArr ? JSON.parse(storedTodoArr) : [];
+  });
+  const [isEdit, setIsEdit] = useState(false);
+  const [editedTaskId, setEditedTaskId] = useState(null);
+
+  // Save tasks to local storage whenever the todoArr changes
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todoArr));
+  }, [todoArr]);
 
   const handleChange = (e) => {
     setInputText(e.target.value);
   };
 
-  const handleButton = (e) => {
-    console.log(inputText);
+  // Add a particular task...
+  const handleAddButton = (e) => {
     if (inputText) {
       setTodoArr((prev) => {
-        const newArr = [inputText, ...prev];
+        const newArr = [{ text: inputText, id: Date.now() }, ...prev];
         return newArr;
       });
-      console.log(todoArr);
+      setInputText("");
+      setIsEdit(false);
     }
+  };
+
+  /// Handle "Enter" key press
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      if (isEdit) {
+        handleSaveButton();
+      } else {
+        handleAddButton();
+      }
+    }
+  };
+
+  // Delete a particular task...
+  const handleDeleteButton = (e) => {
+    const taskId = Number(e.target.value);
+    setTodoArr((prev) => {
+      const newArr = prev.filter((todo) => todo.id !== taskId);
+      return newArr;
+    });
+  };
+
+  // Edit a particular task...
+  const handleEditButton = (e) => {
+    setIsEdit(true);
+    const taskId = Number(e.target.value);
+    setEditedTaskId(taskId);
+    const taskToEdit = todoArr.find((todo) => todo.id === taskId);
+    setInputText(taskToEdit.text);
+  };
+
+  // Save an edited task...
+  const handleSaveButton = () => {
+    if (editedTaskId !== null && inputText) {
+      setTodoArr((prev) => {
+        return prev.map((todo) => {
+          if (todo.id === editedTaskId) {
+            return { ...todo, text: inputText };
+          }
+          return todo;
+        });
+      });
+      setEditedTaskId(null);
+      setInputText("");
+      setIsEdit(false);
+    }
+  };
+
+  // Delete all tasks at once...
+  const handleDeleteAllTasksButton = () => {
+    setTodoArr([]);
     setInputText("");
   };
 
@@ -24,21 +86,53 @@ const Todo = () => {
     <main>
       <h2>My Todos</h2>
       <div className="input-btn">
-        <input type="text" onChange={handleChange} value={inputText} />
-        <button onClick={handleButton}>Add</button>
+        <input
+          type="text"
+          onChange={handleChange}
+          onKeyPress={handleKeyPress} // Handle "Enter" key press
+          value={inputText}
+        />
+        {!isEdit && <button onClick={handleAddButton}>Add</button>}
       </div>
 
-      {todoArr.map((todo) => {
+      {todoArr.map(({ text, id }) => {
         return (
-          <div className="todos">
-            <p>{todo}</p>
+          <div key={id} className="todos">
+            {isEdit && id === editedTaskId ? (
+              <input
+                className="editTaskInput"
+                type="text"
+                value={inputText}
+                onChange={handleChange}
+                onKeyPress={handleKeyPress} // Handle "Enter" key press
+              />
+            ) : (
+              <p>{text}</p>
+            )}
             <div className="update">
-              <button>Edit</button>
-              <button>Delete</button>
+              {isEdit && id === editedTaskId ? (
+                <button onClick={handleSaveButton} value={id}>
+                  Save
+                </button>
+              ) : (
+                <>
+                  <button onClick={handleEditButton} value={id}>
+                    Edit
+                  </button>
+                  <button onClick={handleDeleteButton} value={id}>
+                    Delete
+                  </button>
+                </>
+              )}
             </div>
           </div>
         );
       })}
+      {todoArr.length > 0 && (
+        <button className="delete-btn" onClick={handleDeleteAllTasksButton}>
+          Delete All Todos
+        </button>
+      )}
     </main>
   );
 };
